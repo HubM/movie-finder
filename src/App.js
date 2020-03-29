1b106327f01abd4fb7971357a9a5b928df0f9006
hubert@webians.fr <hubmoncenis@gmail.com>
2020-03-29 17:54:38 +0200

    :art: remove default svg/css

--- src/App.js
+++ src/App.js
@@ -5,8 +5,6 @@ import {
   Route,
   Link
 } from "react-router-dom";
-import logo from './logo.svg';
-import './App.css';
 
 import Search from "./components/Search";
 import Favorites from "./components/Favorites";
@@ -39,20 +37,6 @@ export default function App() {
             <Profile />
           </Route>
         </Switch>
-        {/* <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header> */}
       </div>
     </Router>
   );
