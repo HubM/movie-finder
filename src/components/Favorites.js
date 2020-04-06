de5d30dda407b46ff52315194d91aa848f6166f6
hubert@webians.fr <hubmoncenis@gmail.com>
2020-04-06 09:16:18 +0200

    :recycle: getDatabase in common method file

--- src/components/Favorites.js
+++ src/components/Favorites.js
@@ -1,6 +1,7 @@
 import React from "react";
-import { openDB,} from 'idb';
 import { withRouter } from "react-router-dom";
+
+import { getDatabase, deleteMovieFromFavorites } from "../helpers/_functions/index";
 
 import movieFinder from "../services/MovieFinder";
 
@@ -17,60 +18,70 @@ class Favorites extends React.Component {
   componentDidMount() {
     this.getFavoritesMovies()
   }
-  getFavoritesMovies = async () => {
-    const db = await openDB('movies', 1);
-    this.setState({
-      db,
-      movies: await db.getAllFromIndex('favorites', 'title')
+
+  getFavoritesMovies = () => {
+    getDatabase()
+    .then(async db => {
+      this.setState({
+        db,
+        movies: await db.getAllFromIndex('favorites', 'title')
+      }) 
+    })
+    .catch(err => {
+      console.error(err);
     }) 
   }
-  deleteMovieFromFavorites = async (movieId) => {
-    const { db } = this.state;
-    const movieKey = await db.getKeyFromIndex("favorites", "movieId", movieId);
-    await db.delete("favorites", movieKey);
-    this.getFavoritesMovies()
+
+  deleteMovieAction = movieId => {
+    deleteMovieFromFavorites(movieId)
+      .then(() => {
+        const movies = this.state.movies.filter(movie => movie.id !== movieId)
+        this.setState({
+          movies
+        })
+      })
+      .catch(err => {
+        console.error(err);
+      })
   }
+
   seeMovieDetails = id => {
     this.props.history.push(`/movie/${id}`);
   }
   render() {
-    const renderFavoritesMoviesSection = () => {
+    const renderFavoritesMoviesSection = (movies) => {
       if (!this.state.indexedDbSupported) {
         return (
           <p>Vous devriez utiliser un navigateur moderne pour pouvoir consulter vos films préférés</p>
         );
       } 
       
-      if (this.state.movies && this.state.movies.length) {
-        const favoritesMovies = this.state.movies.map(movie => {
-          const movieKey = `${movie.movieId}-${movie.release_date}`;
-          const movieImage = this.movieFinder.getImageMovie(movie.poster_path, 500);
-          return (
-            <li className="favorite-movie" key={movieKey}>
-              <img src={movieImage} alt={`Affiche de ${movie.title}`} />
-              <p className="favorite-movie__release">{movie.release_date}</p>
-              <p className="favorite-movie__synopsis">{movie.overview}</p>
-              <button onClick={() => this.deleteMovieFromFavorites(movie.movieId)}>Supprimer des favoris</button>
-              <button onClick={() => this.seeMovieDetails(movie.movieId)}>Détails</button> 
-            </li>
-          )
-        })
-        return (
-          <ul>
-            {favoritesMovies}
-          </ul>
-        );
-      } 
-
       return (
-        <p>Une erreur est survenue, veuillez réessayer ultérieurement</p>
+        <ul>
+          {
+            movies.map(movie => {
+              const movieKey = `${movie.movieId.toString()}-${movie.release_date}`;
+              const movieImage = this.movieFinder.getImageMovie(movie.poster_path, 500);
+              return (
+                <li className="favorite-movie" key={movieKey}>
+                  <img src={movieImage} alt={`Affiche de ${movie.title}`} />
+                  <p className="favorite-movie__release">{movie.release_date}</p>
+                  <p className="favorite-movie__synopsis">{movie.overview}</p>
+                  <button onClick={() => this.deleteMovieAction(movie.movieId)}>Supprimer des favoris</button>
+                  <button onClick={() => this.seeMovieDetails(movie.movieId)}>Détails</button> 
+                </li>
+              )
+            })}
+        </ul>
       );
+
     }
 
     return (
       <section className="favorites-page">
         <h1>Favorites Page</h1>
-        {renderFavoritesMoviesSection()}
+        {this.state.movies && this.state.movies.length === 0 && <p>Aucun film n'est enregistré dans vos favoris</p>}
+        {this.state.movies && this.state.movies.length > 0 && renderFavoritesMoviesSection(this.state.movies)}
       </section>
     );
   }
--- src/helpers/_functions/index.js
+++ src/helpers/_functions/index.js
@@ -1,26 +1,24 @@
 import { openDB } from 'idb';
 
-const openDatabase = async () => {
-  return await openDB('movies', 1, {
-    upgrade(db) {
-      const store = db.createObjectStore('favorites', {
-        keyPath: 'id',
-        autoIncrement: true,
-      });
-      // Create an index on the 'date' property of the objects.
-      store.createIndex('title', 'title');
-      store.createIndex('movieId', 'movieId');
-    },
-  });
-}
+const getDatabase = async () => 
+   await openDB("movies", 1, {
+      upgrade(db, oldVersion, newVersion) {
+        const store = db.createObjectStore('favorites', {
+          keyPath: 'id',
+          autoIncrement: true,
+        });
+        // Create an index on the 'date' property of the objects.
+        store.createIndex('title', 'title');
+        store.createIndex('movieId', 'movieId');
+      }
+    });
 
 const addToFavorite = async (movie) => 
   new Promise((resolve, reject) => {
     const { id, title, overview, poster_path, release_date } = movie;
   
-    openDatabase()
+    getDatabase()
     .then(async db => {
-      console.log(await db.getAllFromIndex('favorites', 'title'))
       const dbMovies = await db.getAllFromIndex('favorites', 'title');
       const existingMovie = dbMovies.find(movie => movie.movieId === id);
     
@@ -38,7 +36,6 @@ const addToFavorite = async (movie) =>
         reject("Movie always in DB");
       }
 
-      
     })
     .catch(err => {
       console.error(err)
@@ -48,20 +45,20 @@ const addToFavorite = async (movie) =>
   
 const deleteMovieFromFavorites = async (movieId) => 
   new Promise((resolve, reject) => {
-    openDatabase()
+    getDatabase()
     .then(async db => {
       const movieKey = await db.getKeyFromIndex("favorites", "movieId", movieId);
       await db.delete("favorites", movieKey);
       resolve();
     })
     .catch(err => {
-      console.error(err)
       reject(err);
     })
   })
 
 
 export {
+  getDatabase,
   addToFavorite,
   deleteMovieFromFavorites  
 }
