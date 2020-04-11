f327b2180d609d24da41f50120cb0c13b1b9020d
hubert@webians.fr <hubmoncenis@gmail.com>
2020-04-11 19:14:06 +0200

    :lipstick: design search input + add loading and interactivity

---
+++ src/components/Loading.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default (props) => 
+  (
+    <svg xmlns="http://www.w3.org/2000/svg" className="loading" width={props.width} height={props.height} viewBox="0 0 100 100">
+      <circle cx="50" cy="23" r="13" fill="#ffffff">
+        <animate attributeName="cy" dur="2s" repeatCount="indefinite" calcMode="spline" keySplines="0.45 0 0.9 0.55;0 0.45 0.55 0.9" keyTimes="0;0.5;1" values="23;77;23"></animate>
+      </circle>
+    </svg>
+  )
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { withRouter } from "react-router-dom";
 
+import Loading from "./Loading";
+
 import { getDatabase, addToFavorite, deleteMovieFromFavorites } from "../helpers/_functions/index"
 
 import movieFinder from "../services/MovieFinder";
@@ -12,7 +14,8 @@ class Search extends React.Component {
       db: undefined,
       search: "",
       movies: [],
-      indexedDbSupported: ('indexedDB' in window)
+      indexedDbSupported: ('indexedDB' in window),
+      loading: false
     }
     this.movieFinder = new movieFinder();
   }
@@ -27,27 +30,36 @@ class Search extends React.Component {
       })
   }
 
-
   keyPress = (event) => event.keyCode === 13 ? this.searchMovie() : false;
 
   searchMovie = () => {
-    if (this.state.search && this.state.search.length > 2) {
-      this.movieFinder.searchMovie(this.state.search)
-        .then(async movies => {
-          const dbMovies = await this.state.db.getAllFromIndex('favorites', 'title');
-          const moviesWithFavorisInfos = movies.map(movie => {
-            return {
-              ...movie,
-              inFav: dbMovies.some(dbMovie => dbMovie.movieId === movie.id)
-            }
+    if (this.state.search.length > 2) {
+      this.setState({
+        loading: true
+      })
+      setTimeout(() => {
+        this.movieFinder.searchMovie(this.state.search)
+          .then(async movies => {
+            const dbMovies = await this.state.db.getAllFromIndex('favorites', 'title');
+            const moviesWithFavorisInfos = movies.map(movie => {
+              return {
+                ...movie,
+                inFav: dbMovies.some(dbMovie => dbMovie.movieId === movie.id)
+              }
+            })
+            this.setState({
+              movies: moviesWithFavorisInfos,
+            })
           })
-          this.setState({
-            movies: moviesWithFavorisInfos
+          .catch(error => {
+            console.error(error)
           })
-        })
-        .catch(error => {
-          console.error(error)
-        })
+          .finally(() => (
+            this.setState({
+              loading: false
+            })
+          ))
+      }, 1000)
     }
   }
   
@@ -85,18 +97,43 @@ class Search extends React.Component {
     this.props.history.push(`/movie/${id}`);
   }
 
+  searchResetState = () => {
+    if (this.state.loading) {
+      return (
+        <Loading width="25px" heigth="25px" />
+      )
+    } else {
+      return (
+        <div 
+          className={`reset-search ${this.state.search.length <= 2 ? "disabledBtn" : ""}`} 
+          onClick={() => this.setState({ movies: [], search: "" })}
+        >
+          <span>+</span>
+        </div>
+      );
+    }
+  }
+
   render() {
     return (
       <section className="search-page">
-        <h1>Search Page</h1>
-        <label htmlFor="movie-search">Rechercher un film:</label>
-        <input 
-          type="text" 
-          aria-label="Rechercher un film" 
-          onChange={(event) => this.setState({ search: event.target.value })} 
-          onKeyDown={this.keyPress}
-        />
-        <button onClick={this.searchMovie}>Rechercher</button>
+        <h1>Rechercher un film</h1>
+        <div className="search">
+          <div>
+            <input 
+              type="text" 
+              aria-label="Bouton de recherche d'un film" 
+              placeholder="Shutter island..."
+              onChange={(event) => this.setState({ search: event.target.value })} 
+              onKeyDown={this.keyPress}
+              value={this.state.search}
+              className={`search-input ${this.state.search.length > 2 ? "usable" : ""}`}
+            />
+            
+            {this.searchResetState()}
+          </div>
+          <button onClick={this.searchMovie} className={`search-action ${this.state.search.length <= 2 ? "disabledBtn" : ""}`}>Rechercher</button>
+        </div>
 
         { !this.state.indexedDbSupported && <p>Vous devriez utiliser un navigateur moderne pour pouvoir enregistrer vos films préférés</p> }
         { this.state.movies.length > 0 && this.generateMoviesList(this.state.movies) }
