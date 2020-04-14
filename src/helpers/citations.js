71ec62b10c4b8016cba9097489a0491793e065eb
hubert@webians.fr <hubmoncenis@gmail.com>
2020-04-14 09:28:18 +0200

    :art: add citations

--- src/helpers/citations.js
+++ src/helpers/citations.js
@@ -42,5 +42,84 @@ export default [
   {
     citation: "Les images seules ne sont rien, seul le montage les convertit en vérité ou en mensonge.",
     author: "Jean Epstein"
+  },
+  {
+    citation: "Tu regardes la télé parce que tu t'emmerdes. Tu vas au cinéma parce que tu cherches quelque chose.",
+    author: "Albert Dupontel"
+  },
+  {
+    citation: "Le cinéma, c'est un désir très fort de marier l'image au son.",
+    author: "David Lynch"
+  },
+  {
+    citation: "La beauté du cinéma, c'est de pouvoir tenter quelque chose de différent.",
+    author: "Clint Eastwood"
+  },
+  {
+    citation: "La télévision fabrique de l'oubli. Le cinéma fabrique des souvenirs.",
+    author: "Jean-Luc Godard"
+  },
+  {
+    citation: "Le cinéma, c'est l'écriture moderne dont l'encre est la lumière.",
+    author: "Jean Cocteau"
+  },
+  {
+    citation: "Au cinéma, le mauvais temps est terriblement photogénique, parce qu'il est dangereux.",
+    author: "Claude Lelouch"
+  },
+  {
+    citation: "Quand les gens me demandent si je suis allé à l'école de cinéma, je leur dis: Non, je suis allé au cinéma.",
+    author: "Quentin Tarantino"
+  },
+  {
+    citation: "Le cinéma est un mélange parfait de vérité et de spectacle.",
+    author: "François Truffaut"
+  }, 
+  {
+    citation: "Le cinéma substitue à nos regards un monde qui s'accorde à nos désirs.",
+    author: "André Bazin"
+  },
+  {
+    citation: "Je préfère idéaliser le réel, sinon pourquoi aller au cinéma ?",
+    author: "Jacques Demy"
+  },
+  {
+    citation: "Le cinéma est le plus totalitaire des arts.",
+    author: "Jim Morrison"
+  },
+  {
+    citation: "Le cinéma, c'est un stylo, du papier et des heures à observer le monde et les gens.",
+    author: "Jacques Tati"
+  },
+  {
+    citation: "Le cinéma, très tôt, a prouvé qu'il valait mieux cacher que montrer.",
+    author: "Jacques Audiard"
+  },
+  {
+    citation: "Le cinéma, c'est une parenthèse enchantée.",
+    author: "Fabrice Luchini"
+  },    
+  {
+    citation: "J'ai été le plus grand cocu de l'histoire du cinéma français.",
+    author: "Bernard Blier"
+  },
+  {
+    citation: "Le but ce n'est pas de faire du cinéma, mais son cinéma.",
+    author: "Albert Dupontel"
+  },
+  {
+    citation: "La bande dessinée, c'est comme le cinéma, même si c'est un cinéma de pauvres.",
+    author: "Hugo Pratt"
+  },
+  {
+    citation: "Le cinéma, comme la peinture, montre l'invisible.",
+    author: "Jean-Luc Godard"
+  },
+  {
+    citation: "Le cinéma est pour moi un art tridimensionnel. Avec ma caméra, j'ai le sentiment de sculpter l'espace.",
+    author: "David Cronenberg"
+  },  {
+    citation: "Les images muettes étaient la forme la plus pure de cinéma.",
+    author: "Alfred Hitchcock"
   }
 ]
