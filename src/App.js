0e067dab9320b92df56c38cfe8bda2fd19cc745f
hubert@webians.fr <hubmoncenis@gmail.com>
2020-04-14 16:53:19 +0200

    :zap: using lazy loads and suspense for components

--- src/App.js
+++ src/App.js
@@ -1,36 +1,38 @@
-import React from 'react';
+import React,  { lazy, Suspense } from 'react';
 import {
   BrowserRouter as Router,
   Switch,
   Route,
 } from "react-router-dom";
 
-import Layout from "./components/Layout";
-import Home from "./components/views/Home";
-import Search from "./components/views/Search";
-import Favorites from "./components/views/Favorites";
-import Movie from "./components/views/Movie";
+const Layout = lazy(() => import("./components/Layout"));
+const Home = lazy(() => import("./components/views/Home"))
+const Search = lazy(() => import("./components/views/Search"))
+const Favorites = lazy(() => import("./components/views/Favorites"))
+const Movie = lazy(() => import("./components/views/Movie"))
 
 
 export default function App() {
   return (
     <Router>
-      <div className="App">
-        <Layout>
-          <Switch>
-            <Route path="/search">
-              <Search />
-            </Route>
-            <Route path="/favorites">
-              <Favorites />
-            </Route>
-            <Route path="/movie/:id" children={<Movie />} />
-            <Route path="/">
-              <Home />
-            </Route>
-          </Switch>
-        </Layout>
-      </div>
+      <Suspense fallback={<div>Loading...</div>}>
+        <div className="App">
+          <Layout>
+            <Switch>
+              <Route path="/search">
+                <Search />
+              </Route>
+              <Route path="/favorites">
+                <Favorites />
+              </Route>
+              <Route path="/movie/:id" children={<Movie />} />
+              <Route path="/">
+                <Home />
+              </Route>
+            </Switch>
+          </Layout>
+        </div>
+      </Suspense>
     </Router>
   );
 }
